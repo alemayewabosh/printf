2ff8d6d04de3c1b5f97264aeab0facf2c09969b4
Alemayew Abate <alemayew.abonesh07@gmail.com>
2022-05-20 23:23:27 +0300

    all commited

--- _printf.c
+++ _printf.c
@@ -1,182 +1,82 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdint.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * unsigned_integer_to_string - changes the unsigned integer to string
- * @integer: integer number
- * @base: takes in the desired base to print with
- * @buffer: charachter array
- * Return: returns nothing
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
  */
-
-void unsigned_integer_to_string(uint64_t integer, int base, char *buffer)
+static int (*check_for_specifiers(const char *format))(va_list)
 {
-	int i, digit, cur = 0;
-	char _buffer[40];
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
 
-	if (integer == 0)
+	for (i = 0; p[i].t != NULL; i++)
 	{
-		*buffer++ = '0';
-		*buffer = 0;
-		return;
-	}
-	for (i = 0; i < 40; i++)
-		_buffer[i] = 0;
-	while (integer)
-	{
-		digit = integer % base;
-		if (digit >= 10)
-			_buffer[cur++] = 'a' + (digit - 10);
-		else if (digit >= 10)
-			_buffer[cur++] = 'A' + (digit - 10);
-		else
-			_buffer[cur++] = '0' + digit;
-		integer /= base;
-	}
-
-	for (i = cur - 1; i != 0; i--)
-		*buffer++ = _buffer[i];
-	*buffer++ = _buffer[0];
-	*buffer = 0;
-}
-
-/**
- * signed_integer_to_string - changes the signed integer to string
- * @integer: integer number
- * @base: takes in the desired base to print with
- * @buffer: charachter array
- * Return: returns nothing
- */
-
-void signed_integer_to_string(int64_t integer, int base, char *buffer)
-{
-	if (integer < 0)
-	{
-		*buffer++ = '-';
-		integer = -integer;
-	}
-	unsigned_integer_to_string(integer, base, buffer);
-}
-
-/**
- * switchFunction - switchs cases of format specifiers
- * @format: is a character string
- * @args: takes in list of arguments
- * Return: returns nothing
- */
-
-/*State: 0: regular 1: escape*/
-
-void switchFunction(const char *format, va_list args)
-{
-	const char *s;
-
-	switch (*format)
-	{
-	case 'c':
-		_putchar((va_arg(args, int)));
-		break;
-	case 's':
-	{
-		s = va_arg(args, const char *);
-		while (*s)
-			_putchar(*s++);
-		break;
-	}
-	case '%':
-		_putchar('%');
-		break;
-	case 'd':
-		printBuffer(10, args);
-		break;
-	case 'i':
-		printBuffer(10, args);
-		break;
-	case 'b':
-		printBuffer(2, args);
-		break;
-	default:
-	break;
-	}
-}
-
-/**
- * v_printf - checks if a percentage specifier is
- *            there and prints
- * @format: is a character string
- * @args: takes in list of arguments
- * Return: returns nothing
- */
-
-void v_printf(const char *format, va_list args)
-{
-	int state = 0;
-
-	while (*format)
-	{
-		if (state == 0)
+		if (*(p[i].t) == *format)
 		{
-			if (*format == '%')
-				state = 1;
-			else
-				_putchar(*format);
-		}
-		else if (state == 1)
-		{
-			switchFunction(format, args);
-			state = 0;
-		}
-		format++;
-	}
-}
-
-/**
- * _printf - produces output according to a format
- * @format: is a character string
- * Return: the number of characters printed
- *         (excluding the null byte used to end output to strings)
- */
-
-int _printf(const char *format, ...)
-{
-	int noSpecifier = 1, length = 0, isPercentage = 0;
-	const char *s = format, *s1 = format;
-	va_list args;
-
-	while (*s1)
-	{
-		if (*s1 == '%')
-		{
-			noSpecifier = 0;
 			break;
 		}
-		s1++;
 	}
-	while (*s1)
+	return (p[i].f);
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
+
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
 	{
-		if (*s1 == '%' && *(s1 + 1) == '%')
-			isPercentage = 1;
-		s1++;
-	}
-	va_start(args, format);
-	if (!noSpecifier)
-		v_printf(format, args);
-	else
-	{
-		while (*s)
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			_putchar(*s);
-			s++;
+			_putchar(format[i]);
+			count++;
 		}
-		_strlen(format, isPercentage);
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
-	va_end(args);
-	for (length = 0; *format != '\0'; format++)
-		length++;
-	if (isPercentage)
-		return (length - 1);
-	else
-		return (length);
+	va_end(valist);
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,12 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdint.h>
-#include <stdarg.h>
-void _putchar(char c);
-int _strlen(const char *s, int flag);
-void unsigned_integer_to_string(uint64_t integer, int base, char *buffer);
-void signed_integer_to_string(int64_t integer, int base, char *buffer);
-void printBuffer(int base, va_list args);
-int _printf(const char *format, ...);
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-#endif /*#ifndef MAIN_H*/
+#include <stdarg.h>
+/**
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+
+#endif  /* _MAIN_H */
